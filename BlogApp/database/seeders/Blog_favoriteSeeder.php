d26cf162d46b27bc610baa12f54ce2602a0ac275
kota <pcdradatatinnsave21@gmail.com>
2024-07-23 20:57:08 +0900

    seeder部分の修正

---
+++ BlogApp/database/seeders/Blog_favoriteSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Blog_favoriteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('blog_favorites')->insert([
+            [
+                'id' => 1,
+                'blog_id' => 1,
+                'favorite_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'blog_id' => 2,
+                'favorite_id' => 2,
+            ],
+            [
+                'id' => 3,
+                'blog_id' => 3,
+                'favorite_id' => 3,
+            ],
+        ]);
+    }
+}
--- BlogApp/database/seeders/DatabaseSeeder.php
+++ BlogApp/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
+use Database\Seeders\FavoriteSeeder;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,52 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('blogs')->insert([
-            [            
-                'blog_id' => 1,
-                'title' => 'いいニュース',
-                'explanation' => 'いいニュースの内容',
-            ],
-            [
-                'blog_id' => 2,
-                'title' => '普通のニュース',
-                'explanation' => '普通のニュースの内容',
-            ],
-            [
-                'blog_id' => 3,
-                'title' => '悪いニュース',
-                'explanation' => '悪いニュースの内容',
-            ],
-        ]);
-
-        DB::table('favorites')->insert([
-            [
-                'favorite_id' => 1,
-            ],
-            [
-                'favorite_id' => 2,
-            ],
-            [
-                'favorite_id' => 3,
-            ],
-        ]);
-
-        DB::table('blog_favorites')->insert([
-            [
-                'id' => 1,
-                'blog_id' => 1,
-                'favorite_id' => 1,
-            ],
-            [
-                'id' => 2,
-                'blog_id' => 2,
-                'favorite_id' => 2,
-            ],
-            [
-                'id' => 3,
-                'blog_id' => 3,
-                'favorite_id' => 3,
-            ],
+        $this->call([
+            BlogSeeder::class,
+            FavoriteSeeder::class,
+            Blog_favoriteSeeder::class,
         ]);
     }
 }
