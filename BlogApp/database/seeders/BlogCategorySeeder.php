2045adbec36457b9a9652f0997c44268aaecb185
kota <pcdradatatinnsave21@gmail.com>
2024-07-24 14:34:07 +0900

    追加のテーブル作成と修正

---
+++ BlogApp/database/seeders/BlogCategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BlogCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('blog_categories')->insert([
+            [
+                'id' => 1,
+                'blog_id' => 1,
+                'category_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'blog_id' => 2,
+                'category_id' => 2,
+            ],
+            [
+                'id' => 3,
+                'blog_id' => 3,
+                'category_id' => 3,
+            ],
+        ]);
+    }
+}
